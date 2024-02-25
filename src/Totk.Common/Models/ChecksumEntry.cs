b460ecbdfe8ee35f67c14225b37f107d9f542767
ArchLeaders <archleadership28@gmail.com>
2024-02-25 00:02:55 -0800

    Add binary structs

---
+++ src/Totk.Common/Models/ChecksumEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace Totk.Common.Models;
+
+internal struct ChecksumEntry
+{
+    public ChecksumEntryInfo Info;
+    public int Size;
+    public ulong Checksum;
+}
---
+++ src/Totk.Common/Models/ChecksumEntryInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Totk.Common.Models;
+
+[StructLayout(LayoutKind.Explicit, Pack = 4, Size = 4)]
+internal struct ChecksumEntryInfo
+{
+    [FieldOffset(0)]
+    public int FirstVersionIndex;
+
+    [FieldOffset(0)]
+    public int Version;
+}
