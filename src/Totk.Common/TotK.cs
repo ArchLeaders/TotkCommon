9ba0e4ebedd2b2c2f8f7c7ab3c47b64aa8e89631
ArchLeaders <archleadership28@gmail.com>
2024-04-25 21:54:18 -0700

    Create TotK config/root class

---
+++ src/Totk.Common/TotK.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Totk.Common;
+
+public class TotK
+{
+    private static readonly string _path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "totk", "Config.json");
+
+    public static TotK Config { get; }
+    public static Zstd Zstd { get; }
+
+    static TotK()
+    {
+        if (!File.Exists(_path)) {
+            Config = new();
+        }
+
+        using FileStream fs = File.OpenRead(_path);
+        Config = JsonSerializer.Deserialize<TotK>(fs)
+            ?? new();
+
+        Zstd = new();
+        Zstd.LoadDictionaries(Config.ZsDicPath);
+    }
+
+    private string _gamePath = string.Empty;
+    public string GamePath {
+        get => _gamePath;
+        set {
+            _gamePath = value;
+            Zstd.LoadDictionaries(ZsDicPath);
+        }
+    }
+
+    [JsonIgnore]
+    public string ZsDicPath => Path.Combine(GamePath, "Pack", "ZsDic.pack.zs");
+
+    public void Save()
+    {
+        Directory.CreateDirectory(Path.GetDirectoryName(_path)!);
+        using FileStream fs = File.Create(_path);
+        JsonSerializer.Serialize(fs, this);
+    }
+}
+
