e17415fa47c490280614ddf22d026a3f21d3eb24
ArchLeaders <archleadership28@gmail.com>
2024-06-13 11:49:08 -0700

    Store AddressTable

--- src/TotkCommon/Totk.cs
+++ src/TotkCommon/Totk.cs
@@ -1,4 +1,9 @@
-﻿using System.Text.Json;
+﻿using BymlLibrary;
+using BymlLibrary.Extensions;
+using CommunityToolkit.HighPerformance.Buffers;
+using Revrs;
+using System.Collections.Frozen;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using TotkCommon.Extensions;
 
@@ -10,6 +15,7 @@ public class Totk
 
     public static Totk Config { get; }
     public static Zstd Zstd { get; }
+    public static FrozenDictionary<string, string>? AddressTable { get; private set; }
 
     static Totk()
     {
@@ -30,17 +36,23 @@ public class Totk
         get => _gamePath;
         set {
             _gamePath = value;
+            Version = GamePath.GetRomfsVersionOrDefault(100);
 
             if (File.Exists(ZsDicPath)) {
                 Zstd.LoadDictionaries(ZsDicPath);
             }
 
-            Version = GamePath.GetRomfsVersionOrDefault(100);
+            if (File.Exists(AddressTablePath)) {
+                BuildAddressTable();
+            }
         }
     }
 
     [JsonIgnore]
     public string ZsDicPath => Path.Combine(GamePath, "Pack", "ZsDic.pack.zs");
+
+    [JsonIgnore]
+    public string AddressTablePath => Path.Combine(GamePath, "System", "AddressTable", $"Product.{Version}.Nin_NX_NVN.atbl.byml.zs");
 
     [JsonIgnore]
     public int Version { get; private set; } = 100;
@@ -50,6 +62,28 @@ public class Totk
         Directory.CreateDirectory(Path.GetDirectoryName(_path)!);
         using FileStream fs = File.Create(_path);
         JsonSerializer.Serialize(fs, this, TotkConfigSerializerContext.Default.Totk);
+    }
+
+    private void BuildAddressTable()
+    {
+        using FileStream fs = File.OpenRead(AddressTablePath);
+        int size = Convert.ToInt32(fs.Length);
+        using SpanOwner<byte> buffer = SpanOwner<byte>.Allocate(size);
+        fs.Read(buffer.Span);
+
+        size = Zstd.GetDecompressedSize(buffer.Span);
+        using SpanOwner<byte> decompressed = SpanOwner<byte>.Allocate(size);
+        Zstd.Decompress(buffer.Span, decompressed.Span);
+
+        Dictionary<string, string> addressTable = [];
+
+        RevrsReader reader = new(decompressed.Span);
+        ImmutableByml root = new(ref reader);
+        foreach (var (keyIndex, value) in root.GetMap()) {
+            addressTable[root.KeyTable[keyIndex].ToManaged()] = root.StringTable[value.GetStringIndex()].ToManaged();
+        }
+
+        AddressTable = addressTable.ToFrozenDictionary();
     }
 }
 
